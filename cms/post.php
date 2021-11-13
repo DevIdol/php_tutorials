03542c59be08193b0afdee19127e7502b008f87a
DevIdol <devidol.mm@gmail.com>
2021-11-13 09:16:03 +0630

    modified commit

--- cms/post.php
+++ cms/post.php
@@ -22,7 +22,7 @@
                 $result = mysqli_query($connect,$query);
 
                 if(!$result){
-                    die('Query Failed ' . mysqli_error($result));
+                    die('Query Failed ' . mysqli_error($connect));
                 }
 
                 while($row = mysqli_fetch_assoc($result)){
